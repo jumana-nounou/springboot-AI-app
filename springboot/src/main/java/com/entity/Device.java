b874d05a08f4bdf5416ce847d10357d9ea049cc6
Jumana <jumananounou@outlook.com>
2024-05-03 18:35:29 +0300

    Add Models

--- springboot/src/main/java/com/entity/Device.java
+++ springboot/src/main/java/com/entity/Device.java
@@ -1,3 +1,5 @@
+package com.entity;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -31,4 +33,146 @@ public class Device {
     private Boolean wifi;
     private Integer priceRange;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getBatteryPower() {
+        return batteryPower;
+    }
+
+    public void setBatteryPower(Integer batteryPower) {
+        this.batteryPower = batteryPower;
+    }
+    public Boolean getFourG() {
+        return fourG;
+    }
+
+    public void setFourG(Boolean fourG) {
+        this.fourG = fourG;
+    }
+
+    public Integer getIntMemory() {
+        return intMemory;
+    }
+
+    public void setIntMemory(Integer intMemory) {
+        this.intMemory = intMemory;
+    }
+
+    public Double getMDep() {
+        return mDep;
+    }
+
+    public void setMDep(Double mDep) {
+        this.mDep = mDep;
+    }
+
+    public Integer getMobileWt() {
+        return mobileWt;
+    }
+
+    public void setMobileWt(Integer mobileWt) {
+        this.mobileWt = mobileWt;
+    }
+
+    public Integer getNCores() {
+        return nCores;
+    }
+
+    public void setNCores(Integer nCores) {
+        this.nCores = nCores;
+    }
+
+    public Integer getPc() {
+        return pc;
+    }
+
+    public void setPc(Integer pc) {
+        this.pc = pc;
+    }
+
+    public Integer getPxHeight() {
+        return pxHeight;
+    }
+
+    public void setPxHeight(Integer pxHeight) {
+        this.pxHeight = pxHeight;
+    }
+
+    public Integer getPxWidth() {
+        return pxWidth;
+    }
+
+    public void setPxWidth(Integer pxWidth) {
+        this.pxWidth = pxWidth;
+    }
+
+    public Integer getRam() {
+        return ram;
+    }
+
+    public void setRam(Integer ram) {
+        this.ram = ram;
+    }
+
+    public Integer getScH() {
+        return scH;
+    }
+
+    public void setScH(Integer scH) {
+        this.scH = scH;
+    }
+
+    public Integer getScW() {
+        return scW;
+    }
+
+    public void setScW(Integer scW) {
+        this.scW = scW;
+    }
+
+    public Integer getTalkTime() {
+        return talkTime;
+    }
+
+    public void setTalkTime(Integer talkTime) {
+        this.talkTime = talkTime;
+    }
+
+    public Boolean getThreeG() {
+        return threeG;
+    }
+
+    public void setThreeG(Boolean threeG) {
+        this.threeG = threeG;
+    }
+
+    public Boolean getTouchScreen() {
+        return touchScreen;
+    }
+
+    public void setTouchScreen(Boolean touchScreen) {
+        this.touchScreen = touchScreen;
+    }
+
+    public Boolean getWifi() {
+        return wifi;
+    }
+
+    public void setWifi(Boolean wifi) {
+        this.wifi = wifi;
+    }
+
+    public Integer getPriceRange() {
+        return priceRange;
+    }
+
+    public void setPriceRange(Integer priceRange) {
+        this.priceRange = priceRange;
+    }
 }
